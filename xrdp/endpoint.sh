47eaf2787a4aba3b387231c58f27a22dcb9ad6bf
yama07 <n.116@icloud.com>
2017-08-26 14:57:09 +0000

    allow the user to change xrdp login-name

--- xrdp/endpoint.sh
+++ xrdp/endpoint.sh
@@ -2,31 +2,32 @@
 
 USER_ID=$(id -u)
 GROUP_ID=$(id -g)
+PASSWD=${PASSWD:-${DEFAULT_PASSWD}}
+USER=${USER:-${DEFAULT_USER}}
 
 # Generate container group
 echo "GROUP_ID: $GROUP_ID"
 if [ x"$GROUP_ID" != x"0" ]; then
-    groupadd -g $GROUP_ID $USER_NAME
+    groupadd -g $GROUP_ID $USER
 fi
 
 # Generate container user
 echo "USER_ID: $USER_ID"
 if [ x"$USER_ID" != x"0" ]; then
-    useradd -d $HOME -s /bin/bash -u $USER_ID -g $GROUP_ID $USER_NAME
+    useradd -d /home/$USER -m -s /bin/bash -u $USER_ID -g $GROUP_ID $USER
 fi
 
 # Revert permissions
 sudo chmod u-s /usr/sbin/useradd
 sudo chmod u-s /usr/sbin/groupadd
 
-# Set HOME dir permission
-echo "HOME: ${HOME}"
-sudo chown ${USER_ID}:${GROUP_ID} -R $HOME
-sudo chmod 755 -R $HOME
+# Set login use rname
+USER=$(whoami)
+echo "USER: $USER"
 
-# Change password
+# Set login password
 echo "PASSWD: $PASSWD"
-echo `whoami`:${PASSWD} | sudo chpasswd
+echo ${USER}:${PASSWD} | sudo chpasswd
 
 echo "#############################"
 
